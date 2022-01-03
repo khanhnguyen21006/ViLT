73f959c9017702d970f267d0a61e735537af395c
khanhnv <khanhnv@saltlux.com>
2022-01-03 19:25:06 +0100

    save checkpoints after specific training steps

--- vilt/modules/tat_utils.py
+++ vilt/modules/tat_utils.py
@@ -8,6 +8,8 @@ import torch.nn.functional as F
 from collections import defaultdict
 from typing import Dict
 
+import pytorch_lightning as pl
+
 
 class SinusoidalPositionalEmbedding(nn.Module):
     """Construct sinusoidal positional embeddings of any length.
@@ -226,3 +228,40 @@ def softmax(x, dim, onnx_trace=False):
         return F.softmax(x.float(), dim=dim)
     else:
         return F.softmax(x, dim=dim, dtype=torch.float32)
+
+
+class CheckpointEveryNSteps(pl.Callback):
+    """
+    Save a checkpoint every N steps, instead of Lightning's default that checkpoints
+    based on validation loss.
+    """
+
+    def __init__(
+        self,
+        save_step_frequency,
+        prefix="N-Step-Checkpoint",
+        use_modelcheckpoint_filename=False,
+    ):
+        """
+        Args:
+            save_step_frequency: how often to save in steps
+            prefix: add a prefix to the name, only used if
+                use_modelcheckpoint_filename=False
+            use_modelcheckpoint_filename: just use the ModelCheckpoint callback's
+                default filename, don't use ours.
+        """
+        self.save_step_frequency = save_step_frequency
+        self.prefix = prefix
+        self.use_modelcheckpoint_filename = use_modelcheckpoint_filename
+
+    def on_batch_end(self, trainer: pl.Trainer, _):
+        """ Check if we should save a checkpoint after every train batch """
+        epoch = trainer.current_epoch
+        global_step = trainer.global_step
+        if global_step % self.save_step_frequency == 0:
+            if self.use_modelcheckpoint_filename:
+                filename = trainer.checkpoint_callback.filename
+            else:
+                filename = f"{self.prefix}_epoch-{epoch}_global_step-{global_step}.ckpt"
+            ckpt_path = os.path.join(trainer.checkpoint_callback.dirpath, filename)
+            trainer.save_checkpoint(ckpt_path)
